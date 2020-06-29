ab4d38d7b0dc2f8091f2af01e8e0d3dd10b5d0c6
Ekwuno Obinna <obinnacodes@gmail.com>
2020-06-29 11:23:12 +0100

    add darkmode for accessibility

--- src/components/container.js
+++ src/components/container.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
 export default ({ children }) => (
-  <div style={{ Width: 2000, margin: '0 auto'}}>{children}</div>
+  <div 
+  style={{ 
+    Width: 2000,
+    margin: '0 auto', 
+    backgroundColor: 'var(--bg)',
+    color: 'var(--textNormal)',
+    transition: 'color 0.2s ease-out,background 0.2s ease-out',
+  }}>
+    
+    {children}
+    
+    </div>
 )
